ab68dd86cb6485ae0031ee4346a966ab44070040
Simon <a20simst@student.his.se>
2021-04-26 10:52:40 +0200

    Added classes for JSON, added GSON library

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            InputStream is = getApplicationContext().getAssets().open("mountains.json");
+            String s = convertStreamToString(is);
+            Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+        }catch (Exception e){
+            Log.e("MainActivity","Something went wrong when reading textfile:\n\n"+ e.getMessage());
+        }
     }
 }
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,34 @@
+package com.example.networking;
+
+
+/* {
+         "ID": "mobilprog_matterhorn",
+         "name": "Matterhorn",
+         "type": "brom",
+         "company": "",
+         "location": "The Alps",
+         "category": "",
+         "size": 4478,
+         "cost": 14692,
+         "auxdata": {
+         "wiki": "https://en.wikipedia.org/wiki/Matterhorn",
+         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Matterhorn_from_Domhütte_-_2.jpg/640px-Matterhorn_from_Domhütte_-_2.jpg"
+         }
+
+ */
+
+public class Mountain {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private int size;
+    private int cost;
+    private Auxdata auxdata;
+
+
+
+
+}
