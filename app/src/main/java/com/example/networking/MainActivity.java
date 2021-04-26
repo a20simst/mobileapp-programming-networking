86abc3cc89fd749451ef8823bc27ab3cb880cb3d
Simon <a20simst@student.his.se>
2021-04-26 11:01:45 +0200

    Added a Mountain array variabel to MainActivity and created a Gson object instance. added the information from the json code to the the array variable.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    private Mountain[] mountains;
 
     public static String convertStreamToString(InputStream is) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
@@ -31,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
             InputStream is = getApplicationContext().getAssets().open("mountains.json");
             String s = convertStreamToString(is);
             Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+
+            Gson gson = new Gson();
+            mountains = gson.fromJson(s,Mountain[].class);
         }catch (Exception e){
             Log.e("MainActivity","Something went wrong when reading textfile:\n\n"+ e.getMessage());
         }
